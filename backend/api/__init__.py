266e742226ef26777805f5fb32154f11684287be
kobiethomas983 <kobiethomas983@gmail.com>
2025-04-07 15:10:19 -0400

    seed database with json data

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,7 +1,9 @@
 import os
+import json
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemyError
 from flask_restx import Api
 
 
@@ -21,7 +23,31 @@ def create_app():
     api.add_namespace(course_api, path='/courses')
     
     with app.app_context():
+        from .models import Course
+        db.drop_all() #remove previous data
         db.create_all()
+
+        try:
+            with open('course.json', 'r') as file:
+                json_courses = json.load(file)
+                for jc in json_courses:
+                    course = Course(
+                        title=jc['title'],
+                        author=jc['author'],
+                        free=jc['free'],
+                        overview=jc['overview'],
+                        img=jc['img'],
+                        url=jc['url']
+                    )
+                    db.session.add(course)
+                    db.session.commit()
+        except SQLAlchemyError as error:
+            raise(f"Error: issues saving {error}")
+        except FileNotFoundError:
+            raise("Error: File not found")
+        except json.JSONDecodeError:
+            raise("Error: Invalid Json format")
+        
     
     return app
 
