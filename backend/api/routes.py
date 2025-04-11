2190801abd8cace036bcc00b10c358cc81047f1e
kobiethomas983 <kobiethomas983@gmail.com>
2025-04-10 23:54:04 -0400

    add search query;

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,5 +1,6 @@
 from flask import jsonify
-from flask_restx import Resource
+from flask_restx import Resource, reqparse
+from sqlalchemy.exc import SQLAlchemyError
 
 from .api_models import api
 from .models import Course
@@ -7,6 +8,9 @@ from .models import Course
 from . import db
 
 course_api = api
+
+search_parser = reqparse.RequestParser()
+search_parser.add_argument('q', type=str, required=True)
 
 @course_api.route("")
 class Courses(Resource):
@@ -22,5 +26,28 @@ class SingleCourse(Resource):
         if not course:
             course_api.abort(404, 'course does not exist')
         return jsonify(course.to_dict())
+    
 
+@course_api.route("/search")
+class SearchCourse(Resource):
+    def get(self):
+        query_parameter = search_parser.parse_args().get("q")
+        search = f"%{query_parameter}%"
+
+        try:
+            courses_by_title = Course.query.filter(Course.title.like(search)).all()
+            courses_by_overview = Course.query.filter(Course.overview.like(search)).all()
+        except SQLAlchemyError as error:
+            course_api.abort(500, f"Server error: {error}")
+    
+        total_course_collection = courses_by_title + courses_by_overview
+        unique_courses = []
+        seen = set()
+
+        for crs in total_course_collection:
+            if crs not in seen:
+                seen.add(crs)
+                unique_courses.append(crs)
+
+        return jsonify([c.to_dict() for c in unique_courses])
 
