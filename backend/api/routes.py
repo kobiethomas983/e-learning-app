a96c4f56c2c489933a2d572afa02a3beaad2e562
kobiethomas983 <kobiethomas983@gmail.com>
2025-04-11 22:15:40 -0400

    add pagination and filtering to course get all

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -13,13 +13,48 @@ course_api = api
 search_parser = reqparse.RequestParser()
 search_parser.add_argument('q', type=str, required=True)
 
+pagination_parser = reqparse.RequestParser()
+pagination_parser.add_argument('page', type=int, default=1)
+pagination_parser.add_argument('page_size', type=int, default=10)
+pagination_parser.add_argument('author_filter', type=str, default="")
+
 @course_api.route("")
 class Courses(Resource):
     def get(self):
-        # joinedload does a eagerly fetch on categories so we don't
-        # so we can get all categories for each course all in one sql call
-        courses = Course.query.options(joinedload(Course.categories)).all()
-        return jsonify([course.to_dict(include_categories=True) for course in courses])
+        try: 
+            args = pagination_parser.parse_args()
+            author_filter = args.get("author_filter")
+            page = args.get("page")
+            page_size = args.get("page_size")
+            offset = (page - 1) * page_size
+
+            query = Course.query
+            if author_filter:
+               query = query.filter(Course.author == author_filter)
+
+            # joinedload does a eagerly fetch on categories so we don't
+            # so we can get all categories for each course all in one sql call
+            courses = (
+                        query
+                        .options(joinedload(Course.categories))
+                        .limit(page_size)
+                        .offset(offset)
+                        .all()
+                    )
+            
+            count = query.count()
+            total_pages = (count * page_size - 1) // page_size
+
+            response = {
+                'courses': [course.to_dict(include_categories=True) for course in courses],
+                'page': page,
+                'page_size': page_size,
+                'total_course': count,
+                'total_pages': total_pages
+            }
+            return jsonify(response)
+        except Exception as error:
+            course_api.abort(500, f"error processing request: {error}")
     
 
 @course_api.route("/<id>")
