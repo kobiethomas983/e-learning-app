6192cfc4d64ef08e94164926e83cf1a19c63e33b
kobiethomas983 <kobiethomas983@gmail.com>
2025-05-15 13:36:32 -0400

    add user endpoints

--- backend/api/models/api_models.py
+++ backend/api/models/api_models.py
@@ -4,6 +4,7 @@ course_namespace = Namespace(name="courses", description="Course API")
 categories_namespace =  Namespace(name="categories", description="Categories API")
 admin_namespace = Namespace(name="admin", description="Admin API")
 auth_namespace = Namespace(name="authentication", description="Authentication API")
+user_namespace = Namespace(name="users", description="User API")
 
 category_model = admin_namespace.model(
     'Category',
@@ -31,5 +32,38 @@ login_model = auth_namespace.model(
     {
         "email": fields.String(required=True),
         "password": fields.String(required=True)
+    }
+)
+
+
+create_edit_user_model = user_namespace.model(
+    'User',
+    {
+        "first_name": fields.String(required=True),
+        "last_name": fields.String(required=True),
+        "email": fields.String(required=True),
+        "password": fields.String(required=True),
+        "profile_image": fields.String(),
+        "is_author": fields.Boolean(required=True)
+    }
+)
+
+user_roles_model = user_namespace.model(
+    'User Roles',
+    {
+        'role': fields.String()
+    }
+)
+
+get_user_model = user_namespace.model(
+        'User',
+    {
+        "first_name": fields.String(required=True),
+        "last_name": fields.String(required=True),
+        "email": fields.String(required=True),
+        "password": fields.String(required=True),
+        "profile_image": fields.String(),
+        "is_author": fields.Boolean(required=True),
+        'roles': fields.List(fields.Nested(user_roles_model))
     }
 )
